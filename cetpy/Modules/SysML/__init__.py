c6f67ead481006f36e51b8bfe81c97639b576f71
CongruentEngineeringToolbox <ge36gad@tum.de>
2023-06-30 02:33:13 +0200

    - Removed reference to the not deployed yet feature: Port

--- cetpy/Modules/SysML/__init__.py
+++ cetpy/Modules/SysML/__init__.py
@@ -2,4 +2,3 @@ from cetpy.Modules.SysML._ValueProperty import ValueProperty, value_property, \
     DeterminationTest, AggregateValueProperty, ValuePrinter
 from cetpy.Modules.SysML._PartProperty import PartProperty
 from cetpy.Modules.SysML._Block import Block
-from cetpy.Modules.SysML._Port import Port

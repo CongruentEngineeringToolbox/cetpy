* Fixed sign error on inverse dp calculation of the generic fluid element * Improved robustness of scale_value function

cetpy/Modules/FluidBlock/_GenericFluidBlock.py
@@ -65,7 +65,7 @@ class GenericFluidBlock(FluidBlock):
     def kv(self) -> float:
         """Fluid element flow coefficient [m^3/h]."""
         return self.inlet.vdot * 3600 * np.sqrt(
-            1e5 / self.dp * self.inlet.rho / 1000)
+            -1e5 / self.dp * self.inlet.rho / 1000)
 
     @value_property(determination_test=dp_fixed.determination_test)
     def cd(self) -> float:
@@ -78,7 +78,7 @@ class GenericFluidBlock(FluidBlock):
         if self.__class__.cd.fixed(self) and self.area is not None:
             return self.cd * self.area
         else:
-            return self.inlet.mdot / np.sqrt(self.dp * self.inlet.rho * 2)
+            return self.inlet.mdot / np.sqrt(-self.dp * self.inlet.rho * 2)
 
     @cda.setter
     def cda(self, val: float | None) -> None:
@@ -90,5 +90,5 @@ class GenericFluidBlock(FluidBlock):
     @value_property(determination_test=dp_fixed.determination_test)
     def loss_factor(self) -> float:
         """Fluid element loss coefficient [-]."""
-        return 2 * self.dp / self.inlet.rho / self.inlet.q ** 2
+        return 2 * -self.dp / self.inlet.rho / self.inlet.q ** 2
     # endregion

cetpy/Modules/Utilities/Labelling.py
@@ -243,7 +243,7 @@ standard_unit_magnitudes = np.array([
 
 def scale_value(value: float) -> (float, str):
     """Apply base 3 scaling prefixes to a value and return new value/prefix."""
-    if value == 0:
+    if value == 0 or np.isnan(value) or np.isinf(value):
         index_min = 8
     else:
         index_min = np.where((abs(value) > standard_unit_magnitudes))[0][0]
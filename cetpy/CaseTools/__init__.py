8168912d8164e836983aacbdcb4d9f34b73d2a6b
Ivan Spektorenko <ivan.spektorenko@gmail.com>
2024-10-13 18:22:05 +0200

    Copy CaseRunners test for PCR.
    It is hard to automatically asses, whether multiple cores are being
    used, so the test just checks, if PCR behaves as a CaseRunner should.

--- cetpy/CaseTools/__init__.py
+++ cetpy/CaseTools/__init__.py
@@ -1,5 +1,7 @@
 from cetpy.CaseTools._CaseGenerator import CaseGenerator
 from cetpy.CaseTools._CaseRunner import CaseRunner
 from cetpy.CaseTools._DataSet import DataSet
+from cetpy.CaseTools._ParallelCaseRunner import ParallelCaseRunner
+
 # from cetpy.CaseTools._RegressionDataSet import RegressionDataSet
 # commented out to avoid automatic import of tensorflow.
